207e3e6d4b2d4005d0ef555bd6e3076343204037
sumanadutta23 <129196122+sumanadutta23@users.noreply.github.com>
2023-04-04 23:54:51 +0530

    Add files via upload

---
+++ Login-mvc/src/test/java/com/login/java/LoginMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
